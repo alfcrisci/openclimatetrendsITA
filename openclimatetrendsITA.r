51126c4d446527856d15fc1eba7392f0990f9f54
Alfonso <alfcrisci@gmail.com>
2016-01-26 17:08:34 +0100

    Update openclimatetrendsITA.r

--- openclimatetrendsITA.r
+++ openclimatetrendsITA.r
@@ -24,21 +24,21 @@ tg_ita=brick("tg_ita_eobs_1950_2015.nc",lvar=1)
 rain_ita=brick("rain_ita_eobs_1950_2015.nc",lvar=1)
 
 ##################################################################################################
-# Trend calculation of mean air temperature 
+# Trend calculation of mean air temperature 786 is numer of months for 65.5 years 1950 - 2015
 
 tg_ita_T <- TrendRaster(tg_ita,start=c(1950,1), freq=12,method="SeasonalAdjusted", breaks=0, funSeasonalCycle=MeanSeasonalCycle)
 
 png("trend_T.png")
-plot(tg_ita_T[["SlopeSEG1"]]*736,col=brewer.pal(8,"YlOrRd"))
+plot(tg_ita_T[["SlopeSEG1"]]*786,col=brewer.pal(8,"YlOrRd"))
 plot(italia_bound_region,add=T)
 dev.off()
 
 ##################################################################################################
-# Trend calculation of monthly cumulated rainfall 
+# Trend calculation of monthly cumulated rainfall 786 is numer of months for 65.5 years 1950 - 2015
 
 rain_ita_T <- TrendRaster(rain_ita,start=c(1950,1), freq=12,method="SeasonalAdjusted", breaks=0, funSeasonalCycle=MeanSeasonalCycle)
 
 png("rain_T.png")
-plot(rain_ita_T[["SlopeSEG1"]]*736,col=brewer.pal(8,"RdBu"))
+plot(rain_ita_T[["SlopeSEG1"]]*786,col=brewer.pal(8,"RdBu"))
 plot(italia_bound_region,add=T)
 dev.off()
